ff84e4a08b237b16164d2b2f8e8adb1abac9c154
Tomsar95 <107140629+Tomsar95Laika@users.noreply.github.com>
2022-12-19 13:06:04 -0500

    this commit adds recent series view, with airing endpoint integration and blurred and faded effect for cards

--- lib/features/series/presentation/widgets/favorite_series/favorite_series_scroll_widget.dart
+++ lib/features/series/presentation/widgets/favorite_series/favorite_series_scroll_widget.dart
@@ -5,13 +5,6 @@ import 'package:tv_series/features/series/presentation/widgets/widgets.dart';
 Widget buildFavoritesScroll(List<Series> series, BuildContext context, ScrollController controller) {
   return Flexible(
     child: ListView.builder(
-      /*separatorBuilder: (context, index) => Padding(
-        padding: const EdgeInsets.only(right: 20.0, left: 20),
-        child: Divider(
-          thickness: 2,
-          color: CustomColors.darkGraySemiTransparent,
-        ),
-      ),*/
       shrinkWrap: true,
       scrollDirection: Axis.vertical,
       controller: controller,
